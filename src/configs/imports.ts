1643d6765beafcee6fa3e30ed2a42ae8cc502fb1
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2025-07-11 03:27:20 -0400

    feat: added import rules

---
+++ src/configs/imports.ts
@@ -0,0 +1,40 @@
+import type { OptionsOverrides, OptionsStylistic, TypedFlatConfigItem } from '../types';
+
+import { plugin_ariel, plugin_import } from '../plugins';
+
+export async function imports(options: OptionsOverrides & OptionsStylistic = {}): Promise<TypedFlatConfigItem[]> {
+    const {
+        overrides = {},
+        stylistic = true,
+    } = options
+    
+    return [
+        {
+            name: 'ariel/imports',
+            plugins: {
+                ariel: plugin_ariel,
+                import: plugin_import,
+            },
+            rules: {
+                'ariel/import-dedupe': 'error',
+                'ariel/no-import-dist': 'error',
+                'ariel/no-import-node-modules-by-path': 'error',
+                
+                'import/consistent-type-specifier-style': ['error', 'top-level'],
+                'import/first': 'error',
+                'import/no-duplicates': 'error',
+                'import/no-mutable-exports': 'error',
+                'import/no-named-default': 'error',
+                'import/no-default-export': 'error',
+                
+                ...stylistic
+                ? {
+                    'import/newline-after-import': ['error', { count: 1 }],
+                }
+                : {},
+                
+                ...overrides,
+            },
+        },
+    ]
+};
