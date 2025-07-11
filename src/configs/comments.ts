c7d9bb81204baa96d923756aa34fdae8d410b0d6
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2025-07-11 03:29:06 -0400

    feat: added comments rules

---
+++ src/configs/comments.ts
@@ -0,0 +1,20 @@
+import type { TypedFlatConfigItem } from '../types'
+
+import { plugin_comments } from '../plugins'
+
+export async function comments(): Promise<TypedFlatConfigItem[]> {
+    return [
+        {
+        name: 'ariel/comments',
+        plugins: {
+            'eslint-comments': plugin_comments,
+        },
+        rules: {
+            'eslint-comments/no-aggregating-enable': 'error',
+            'eslint-comments/no-duplicate-disable': 'error',
+            'eslint-comments/no-unlimited-disable': 'error',
+            'eslint-comments/no-unused-enable': 'error',
+        },
+        },
+    ]
+}
