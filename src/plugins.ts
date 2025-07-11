898c34e76816a438968ee54804ac304eefec26e2
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2025-07-11 03:28:49 -0400

    feat(wip): added some plugins

---
+++ src/plugins.ts
@@ -0,0 +1,25 @@
+// @ts-nocheck
+
+/* export * as pluginImport from 'eslint-plugin-import-x'
+export * as pluginJsonc from 'eslint-plugin-jsonc'
+export * as pluginYml from 'eslint-plugin-yml'
+export * as parserYml from 'yaml-eslint-parser'
+export * as parserJsonc from 'jsonc-eslint-parser'
+ */
+export { default as plugin_ariel } from 'eslint-plugin-ariel';
+export { default as plugin_comments } from '@eslint-community/eslint-plugin-eslint-comments';
+export { default as plugin_import } from 'eslint-plugin-import-lite';
+export { default as plugin_unused_imports } from 'eslint-plugin-unused-imports';
+
+/* 
+export { default as pluginMarkdown } from '@eslint/markdown'
+export { default as tseslint } from 'typescript-eslint'
+export { default as pluginUnicorn } from 'eslint-plugin-unicorn'
+export { default as pluginNode } from 'eslint-plugin-n'
+export { default as pluginPerfectionist } from 'eslint-plugin-perfectionist'
+export { default as pluginJsdoc } from 'eslint-plugin-jsdoc'
+export { default as pluginIgnore } from 'eslint-config-flat-gitignore'
+export { default as pluginDeMorgan } from 'eslint-plugin-de-morgan'
+export { default as pluginPnpm } from 'eslint-plugin-pnpm'
+export { default as configCommand } from 'eslint-plugin-command/config' 
+*/
