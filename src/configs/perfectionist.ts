f6d14a71b8cd98ba062b3b5b04f419c9cf238933
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2025-07-12 03:58:37 -0400

    feat: improve sorting of imports and exports

--- src/configs/perfectionist.ts
+++ src/configs/perfectionist.ts
@@ -10,27 +10,61 @@ export async function perfectionist(): Promise<TypedFlatConfigItem[]> {
 				perfectionist: plugin_perfectionist,
 			},
 			rules: {
-				'perfectionist/sort-exports': ['error', { order: 'asc', type: 'line-length' }],
+				'perfectionist/sort-exports': [
+					'error',
+					{
+						order: 'asc',
+						type: 'line-length',
+						groups: [
+							'type-export',
+							'value-export',
+						],
+						newlinesBetween: 1,
+					},
+				],
 				'perfectionist/sort-imports': [
 					'error',
 					{
 						type: 'line-length',
 						order: 'asc',
 						groups: [
-							'type',
+							'type-default',
+							'type-named',
 							'side-effect',
-							'sibling',
-							'parent',
-							'index',
-							'external',
+							'external-default',
+							'external-named',
 							'internal',
+							'parent',
+							'sibling',
+							'index',
+						],
+						customGroups: [
+							{
+								groupName: 'type-default',
+								selector: 'type',
+								modifiers: ['default'],
+							},
+							{
+								groupName: 'type-named',
+								selector: 'type',
+								modifiers: ['named'],
+							},
+							{
+								groupName: 'external-default',
+								selector: 'external',
+								modifiers: ['default'],
+							},
+							{
+								groupName: 'external-named',
+								selector: 'external',
+								modifiers: ['named'],
+							},
 						],
 						newlinesBetween: 'always',
 						sortSideEffects: true,
 					},
 				],
 				'perfectionist/sort-named-exports': ['error', { order: 'asc', type: 'line-length' }],
-				'perfectionist/sort-named-imports': ['error', { order: 'asc', type: 'line-length' }],
 			},
 		},
 	];
