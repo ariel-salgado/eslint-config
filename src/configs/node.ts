e4834b9374f10f9c7791a3cedba083f2b1de5505
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2025-07-12 00:04:52 -0400

    feat: added node rules

---
+++ src/configs/node.ts
@@ -0,0 +1,24 @@
+import type { TypedFlatConfigItem } from '../types';
+
+import { plugin_node } from '../plugins';
+
+export async function node(): Promise<TypedFlatConfigItem[]> {
+	return [
+		{
+			name: 'ariel/node',
+			plugins: {
+				node: plugin_node,
+			},
+			rules: {
+				'node/handle-callback-err': ['error', '^(err|error)$'],
+				'node/no-deprecated-api': 'error',
+				'node/no-exports-assign': 'error',
+				'node/no-new-require': 'error',
+				'node/no-path-concat': 'error',
+				'node/prefer-global/buffer': ['error', 'never'],
+				'node/prefer-global/process': 'off',
+				'node/process-exit-as-throw': 'error',
+			},
+		},
+	];
+}
