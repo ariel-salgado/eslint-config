5f4c08ab825d1234255b769bc0c2fce11cc85f49
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2025-07-12 00:10:24 -0400

    style: eslint

--- src/configs/ignores.ts
+++ src/configs/ignores.ts
@@ -1,15 +1,20 @@
-import type { TypedFlatConfigItem } from '../types'
+import type { TypedFlatConfigItem } from '../types';
 
-import { GLOB_EXCLUDE } from '../globs'
+import { GLOB_EXCLUDE } from '../globs';
+import { plugin_ignore } from '../plugins';
 
 export async function ignores(ignores: string[] = []): Promise<TypedFlatConfigItem[]> {
-    return [
-        {
-            ignores: [
-                ...GLOB_EXCLUDE,
-                ...ignores,
-            ],
-            name: 'ariel/ignores',
-        },
-    ]
+	return [
+		{
+			name: 'ariel/ignores',
+			ignores: [
+				...GLOB_EXCLUDE,
+				...ignores,
+			],
+		},
+		{
+			name: 'ariel/gitignore',
+			...plugin_ignore({ strict: false }),
+		},
+	];
 }
--- src/configs/morgan.ts
+++ src/configs/morgan.ts
@@ -3,16 +3,16 @@ import type { TypedFlatConfigItem } from '../types';
 import { plugin_morgan } from '../plugins';
 
 export async function morgan(): Promise<TypedFlatConfigItem[]> {
-    return [
-        {
-            name: 'ariel/morgan',
-            plugins: {
-                morgan: plugin_morgan
-            },
-            rules: {
-                'morgan/no-negated-conjunction': 'error',
-                'morgan/no-negated-disjunction': 'error'
-            }
-        }
-    ];
+	return [
+		{
+			name: 'ariel/morgan',
+			plugins: {
+				morgan: plugin_morgan,
+			},
+			rules: {
+				'morgan/no-negated-conjunction': 'error',
+				'morgan/no-negated-disjunction': 'error',
+			},
+		},
+	];
 }
