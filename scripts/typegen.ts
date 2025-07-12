351025ab48a6077c3471f2841ef3dce3be23d4e3
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2025-07-12 00:00:59 -0400

    feat: added typegen

--- scripts/typegen.ts
+++ scripts/typegen.ts
@@ -1,15 +1,33 @@
-import { writeFile } from 'node:fs/promises'
-import { flatConfigsToRulesDTS } from 'eslint-typegen/core'
-import { builtinRules } from 'eslint/use-at-your-own-risk'
-
 import {
-	combine,
-	comments,
+	node,
+	pnpm,
+	test,
+	toml,
+	yaml,
+	jsdoc,
+	jsonc,
 	morgan,
+	regexp,
+	svelte,
+	combine,
 	ignores,
 	imports,
-	javascript
+	unicorn,
+	comments,
+	disables,
+	markdown,
+	stylistic,
+	javascript,
+	typescript,
+	tailwindcss,
+	perfectionist,
+	sort_ts_config,
+	sort_package_json,
 } from '../src';
+
+import { writeFile } from 'node:fs/promises';
+import { builtinRules } from 'eslint/use-at-your-own-risk';
+import { flatConfigsToRulesDTS } from 'eslint-typegen/core';
 
 const configs = await combine(
 	{
@@ -20,21 +38,39 @@ const configs = await combine(
 		},
 	},
 	comments(),
-	morgan(),
+	disables(),
 	ignores(),
 	imports(),
-	javascript()
-)
+	javascript(),
+	jsdoc(),
+	jsonc(),
+	markdown(),
+	morgan(),
+	node(),
+	perfectionist(),
+	pnpm(),
+	regexp(),
+	sort_package_json(),
+	sort_ts_config(),
+	stylistic(),
+	svelte(),
+	tailwindcss(),
+	test(),
+	toml(),
+	typescript(),
+	unicorn(),
+	yaml(),
+);
 
-const config_names = configs.map(i => i.name).filter(Boolean) as string[]
+const config_names = configs.map(i => i.name).filter(Boolean) as string[];
 
 let dts = await flatConfigsToRulesDTS(configs, {
 	includeAugmentation: false,
-})
+});
 
 dts += `
 // Names of all the configs
-export type config_names = ${config_names.map(i => `'${i}'`).join(' | ')}
-`
+export type ConfigNames = ${config_names.map(i => `'${i}'`).join(' | ')}
+`;
 
-await writeFile('src/typegen.d.ts', dts)
+await writeFile('src/typegen.d.ts', dts);
