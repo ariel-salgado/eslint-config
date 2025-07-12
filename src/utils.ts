210f8d399a3d401ee30613378bd40f6799d6de5b
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2025-07-12 00:07:30 -0400

    feat: added dependency check support functions

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,74 @@
-import type { Awaitable, TypedFlatConfigItem } from './types'
+import type { Awaitable, TypedFlatConfigItem } from './types';
+
+import { fileURLToPath } from 'node:url';
+import { isPackageExists } from 'local-pkg';
+
+const scope_url = fileURLToPath(new URL('.', import.meta.url));
+const is_cwd_in_scope = isPackageExists('@ariel/eslint-config');
+
+export const parserPlain = {
+	meta: {
+		name: 'parser-plain',
+	},
+	parseForESLint: (code: string) => ({
+		ast: {
+			body: [],
+			comments: [],
+			loc: { end: code.length, start: 0 },
+			range: [0, code.length],
+			tokens: [],
+			type: 'Program',
+		},
+		scopeManager: null,
+		services: { isPlain: true },
+		visitorKeys: {
+			Program: [],
+		},
+	}),
+};
 
 export async function combine(...configs: Awaitable<TypedFlatConfigItem | TypedFlatConfigItem[]>[]): Promise<TypedFlatConfigItem[]> {
-    const resolved = await Promise.all(configs)
-    return resolved.flat()
+	const resolved = await Promise.all(configs);
+	return resolved.flat();
+}
+
+export function rename_rules(
+	rules: Record<string, any>,
+	map: Record<string, string>,
+): Record<string, any> {
+	return Object.fromEntries(
+		Object.entries(rules)
+			.map(([key, value]) => {
+				for (const [from, to] of Object.entries(map)) {
+					if (key.startsWith(`${from}/`))
+						return [to + key.slice(from.length), value];
+				}
+				return [key, value];
+			}),
+	);
+}
+
+export async function interop_default<T>(m: Awaitable<T>): Promise<T extends { default: infer U } ? U : T> {
+	const resolved = await m;
+	return (resolved as any).default || resolved;
+}
+
+export async function ensure_packages(packages: (string | undefined)[]): Promise<void> {
+	if (process.env.CI || process.stdout.isTTY === false || is_cwd_in_scope === false)
+		return;
+
+	const non_existing_packages = packages.filter(i => i && !is_package_in_scope(i)) as string[];
+	if (non_existing_packages.length === 0)
+		return;
+
+	const p = await import('@clack/prompts');
+	const result = await p.confirm({
+		message: `${non_existing_packages.length === 1 ? 'Package is' : 'Packages are'} required for this config: ${non_existing_packages.join(', ')}. Do you want to install them?`,
+	});
+	if (result)
+		await import('@antfu/install-pkg').then(i => i.installPackage(non_existing_packages, { dev: true }));
+}
+
+export function is_package_in_scope(name: string): boolean {
+	return isPackageExists(name, { paths: [scope_url] });
 }
