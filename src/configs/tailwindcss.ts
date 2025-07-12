f49595660a33262ddcf05edbd9eacea16268cd31
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2025-07-12 02:50:44 -0400

    fix: remove css and html from files as it brokes the parser

--- src/configs/tailwindcss.ts
+++ src/configs/tailwindcss.ts
@@ -1,13 +1,13 @@
 import type { OptionsFiles, OptionsOverrides, TypedFlatConfigItem, OptionsHasTailwindCSS } from '../types';
 
-import { GLOB_CSS, GLOB_HTML, GLOB_SVELTE } from '../globs';
+import { GLOB_SVELTE } from '../globs';
 import { ensure_packages, interop_default } from '../utils';
 
 export async function tailwindcss(
 	options: OptionsHasTailwindCSS & OptionsOverrides & OptionsFiles = {},
 ): Promise<TypedFlatConfigItem[]> {
 	const {
-		files = [GLOB_CSS, GLOB_SVELTE, GLOB_HTML],
+		files = [GLOB_SVELTE],
 		overrides = {},
 	} = options;
 
