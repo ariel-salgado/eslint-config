49071842a2f10e618a05d61f688a80534c5210db
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2025-07-12 00:10:06 -0400

    refactor: update exports

--- src/plugins.ts
+++ src/plugins.ts
@@ -1,27 +1,13 @@
+// eslint-disable-next-line ts/ban-ts-comment
 // @ts-nocheck
 
-/* 
-export * as pluginImport from 'eslint-plugin-import-x'
-export * as pluginJsonc from 'eslint-plugin-jsonc'
-export * as pluginYml from 'eslint-plugin-yml'
-export * as parserYml from 'yaml-eslint-parser'
-export * as parserJsonc from 'jsonc-eslint-parser'
-*/
-
+export { default as plugin_node } from 'eslint-plugin-n';
 export { default as plugin_ariel } from 'eslint-plugin-ariel';
-export { default as plugin_comments } from '@eslint-community/eslint-plugin-eslint-comments';
+export { configs as plugin_regexp } from 'eslint-plugin-regexp';
+export { default as plugin_unicorn } from 'eslint-plugin-unicorn';
+export { default as plugin_morgan } from 'eslint-plugin-de-morgan';
 export { default as plugin_import } from 'eslint-plugin-import-lite';
+export { default as plugin_ignore } from 'eslint-config-flat-gitignore';
+export { default as plugin_perfectionist } from 'eslint-plugin-perfectionist';
 export { default as plugin_unused_imports } from 'eslint-plugin-unused-imports';
-export { default as plugin_morgan } from 'eslint-plugin-de-morgan'
-
-/* 
-export { default as pluginMarkdown } from '@eslint/markdown'
-export { default as tseslint } from 'typescript-eslint'
-export { default as pluginUnicorn } from 'eslint-plugin-unicorn'
-export { default as pluginNode } from 'eslint-plugin-n'
-export { default as pluginPerfectionist } from 'eslint-plugin-perfectionist'
-export { default as pluginJsdoc } from 'eslint-plugin-jsdoc'
-export { default as pluginIgnore } from 'eslint-config-flat-gitignore'
-export { default as pluginPnpm } from 'eslint-plugin-pnpm'
-export { default as configCommand } from 'eslint-plugin-command/config' 
-*/
+export { default as plugin_comments } from '@eslint-community/eslint-plugin-eslint-comments';
