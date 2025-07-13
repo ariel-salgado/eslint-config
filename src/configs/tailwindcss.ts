04be5b0a45a3f2fe32e424928953f5ade2618eea
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2025-07-12 21:51:48 -0400

    fix(wip): mantain original plugin name for settings

--- src/configs/tailwindcss.ts
+++ src/configs/tailwindcss.ts
@@ -30,7 +30,7 @@ export async function tailwindcss(
 				...plugin_tailwindcss.configs.recommended.rules,
 			},
 			settings: {
-				tailwindcss: {
+				'better-tailwindcss': {
 					entryPoint,
 				},
 			},
