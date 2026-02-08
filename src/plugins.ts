ba7c44f68b7cdf760c80918fc17d735a6bb91237
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2026-02-08 14:35:30 -0300

    feat: added baseline-js support

--- src/plugins.ts
+++ src/plugins.ts
@@ -1,13 +1,14 @@
 // eslint-disable-next-line ts/ban-ts-comment
 // @ts-nocheck
 
-export { default as plugin_node } from 'eslint-plugin-n';
+export { default as plugin_comments } from '@eslint-community/eslint-plugin-eslint-comments';
+export { default as plugin_ignore } from 'eslint-config-flat-gitignore';
 export { default as plugin_ariel } from 'eslint-plugin-ariel';
-export { configs as plugin_regexp } from 'eslint-plugin-regexp';
-export { default as plugin_unicorn } from 'eslint-plugin-unicorn';
+export { default as plugin_baseline } from 'eslint-plugin-baseline-js';
 export { default as plugin_morgan } from 'eslint-plugin-de-morgan';
 export { default as plugin_import } from 'eslint-plugin-import-lite';
-export { default as plugin_ignore } from 'eslint-config-flat-gitignore';
+export { default as plugin_node } from 'eslint-plugin-n';
 export { default as plugin_perfectionist } from 'eslint-plugin-perfectionist';
+export { configs as plugin_regexp } from 'eslint-plugin-regexp';
+export { default as plugin_unicorn } from 'eslint-plugin-unicorn';
 export { default as plugin_unused_imports } from 'eslint-plugin-unused-imports';
-export { default as plugin_comments } from '@eslint-community/eslint-plugin-eslint-comments';
