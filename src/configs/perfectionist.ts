f8724245f4eab4739743223d4e1797f45342a461
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2026-02-08 14:45:51 -0300

    feat: support for latest api

--- src/configs/perfectionist.ts
+++ src/configs/perfectionist.ts
@@ -14,7 +14,7 @@ export async function perfectionist(): Promise<TypedFlatConfigItem[]> {
 					'error',
 					{
 						order: 'asc',
-						type: 'line-length',
+						type: 'alphabetical',
 						groups: [
 							'type-export',
 							'value-export',
@@ -22,50 +22,105 @@ export async function perfectionist(): Promise<TypedFlatConfigItem[]> {
 						newlinesBetween: 1,
 					},
 				],
+				// OCD ... Sorry ...
 				'perfectionist/sort-imports': [
 					'error',
 					{
-						type: 'line-length',
+						type: 'alphabetical',
 						order: 'asc',
 						groups: [
+							// Types
+							'type-wildcard',
 							'type-default',
 							'type-named',
+
+							// Side effects
+							'side-effect-style',
 							'side-effect',
-							'external-default',
-							'external-named',
-							'internal',
-							'parent',
-							'sibling',
-							'index',
+
+							// Value wildcards
+							'value-wildcard-builtin',
+							{ newlinesBetween: 0 },
+							'value-wildcard-external',
+							{ newlinesBetween: 0 },
+							'value-wildcard-internal',
+							{ newlinesBetween: 0 },
+							'value-wildcard-parent',
+							{ newlinesBetween: 0 },
+							'value-wildcard-sibling',
+							{ newlinesBetween: 0 },
+							'value-wildcard-index',
+							{ newlinesBetween: 1 },
+
+							// Value defaults
+							'value-default-builtin',
+							{ newlinesBetween: 0 },
+							'value-default-external',
+							{ newlinesBetween: 0 },
+							'value-default-internal',
+							{ newlinesBetween: 0 },
+							'value-default-parent',
+							{ newlinesBetween: 0 },
+							'value-default-sibling',
+							{ newlinesBetween: 0 },
+							'value-default-index',
+							{ newlinesBetween: 1 },
+
+							// Value named
+							'value-named-builtin',
+							{ newlinesBetween: 0 },
+							'value-named-external',
+							{ newlinesBetween: 0 },
+							'value-named-internal',
+							{ newlinesBetween: 0 },
+							'value-named-parent',
+							{ newlinesBetween: 0 },
+							'value-named-sibling',
+							{ newlinesBetween: 0 },
+							'value-named-index',
 						],
 						customGroups: [
-							{
-								groupName: 'type-default',
-								selector: 'type',
-								modifiers: ['default'],
-							},
-							{
-								groupName: 'type-named',
-								selector: 'type',
-								modifiers: ['named'],
-							},
-							{
-								groupName: 'external-default',
-								selector: 'external',
-								modifiers: ['default'],
-							},
-							{
-								groupName: 'external-named',
-								selector: 'external',
-								modifiers: ['named'],
-							},
+							// Types
+							{ groupName: 'type-wildcard', selector: 'type', modifiers: ['wildcard'] },
+							{ groupName: 'type-default', selector: 'type', modifiers: ['default'] },
+							{ groupName: 'type-named', selector: 'type', modifiers: ['named'] },
+
+							// Side-effect styles
+							{ groupName: 'side-effect-style', selector: 'side-effect-style' },
+
+							// Side-effects
+							{ groupName: 'side-effect', selector: 'side-effect', modifiers: ['value'] },
+
+							// Value wildcards
+							{ groupName: 'value-wildcard-builtin', selector: 'builtin', modifiers: ['value', 'wildcard'] },
+							{ groupName: 'value-wildcard-external', selector: 'external', modifiers: ['value', 'wildcard'] },
+							{ groupName: 'value-wildcard-internal', selector: 'internal', modifiers: ['value', 'wildcard'] },
+							{ groupName: 'value-wildcard-parent', selector: 'parent', modifiers: ['value', 'wildcard'] },
+							{ groupName: 'value-wildcard-sibling', selector: 'sibling', modifiers: ['value', 'wildcard'] },
+							{ groupName: 'value-wildcard-index', selector: 'index', modifiers: ['value', 'wildcard'] },
+
+							// Value defaults
+							{ groupName: 'value-default-builtin', selector: 'builtin', modifiers: ['value', 'default'] },
+							{ groupName: 'value-default-external', selector: 'external', modifiers: ['value', 'default'] },
+							{ groupName: 'value-default-internal', selector: 'internal', modifiers: ['value', 'default'] },
+							{ groupName: 'value-default-parent', selector: 'parent', modifiers: ['value', 'default'] },
+							{ groupName: 'value-default-sibling', selector: 'sibling', modifiers: ['value', 'default'] },
+							{ groupName: 'value-default-index', selector: 'index', modifiers: ['value', 'default'] },
+
+							// Value named
+							{ groupName: 'value-named-builtin', selector: 'builtin', modifiers: ['value', 'named'] },
+							{ groupName: 'value-named-external', selector: 'external', modifiers: ['value', 'named'] },
+							{ groupName: 'value-named-internal', selector: 'internal', modifiers: ['value', 'named'] },
+							{ groupName: 'value-named-parent', selector: 'parent', modifiers: ['value', 'named'] },
+							{ groupName: 'value-named-sibling', selector: 'sibling', modifiers: ['value', 'named'] },
+							{ groupName: 'value-named-index', selector: 'index', modifiers: ['value', 'named'] },
 						],
 						newlinesBetween: 1,
 						sortSideEffects: true,
 					},
 				],
-				'perfectionist/sort-named-imports': ['error', { order: 'asc', type: 'line-length' }],
-				'perfectionist/sort-named-exports': ['error', { order: 'asc', type: 'line-length' }],
+				'perfectionist/sort-named-imports': ['error', { order: 'asc', type: 'alphabetical' }],
+				'perfectionist/sort-named-exports': ['error', { order: 'asc', type: 'alphabetical' }],
 			},
 		},
 	];
