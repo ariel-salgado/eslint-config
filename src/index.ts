01d3d0009a5df3102d79b70fbc9d860f53d94187
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2026-03-08 14:23:31 -0300

    feat: added presets

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { defineConfig } from './factory';
 export * from './configs';
 export * from './factory';
 export * from './globs';
+export * from './presets';
 export * from './types';
 export * from './utils';
 
---
+++ src/presets.ts
@@ -0,0 +1,56 @@
+import type { OptionsConfig } from './types';
+
+// @keep-sorted
+export const PRESET_FULL_ON: OptionsConfig = {
+	gitignore: true,
+	imports: true,
+	jsdoc: true,
+	jsonc: true,
+	jsx: {
+		a11y: true,
+	},
+	markdown: true,
+	nextjs: true,
+	node: true,
+	pnpm: true,
+	react: {
+		reactCompiler: true,
+	},
+	regexp: true,
+	solid: true,
+	stylistic: {
+		experimental: true,
+	},
+	svelte: true,
+	test: true,
+	toml: true,
+	typescript: {
+		tsconfigPath: 'tsconfig.json',
+	},
+	e18e: true,
+	unicorn: true,
+	yaml: true,
+};
+
+export const PRESET_FULL_OFF: OptionsConfig = {
+	gitignore: false,
+	imports: false,
+	jsdoc: false,
+	jsonc: false,
+	jsx: false,
+	markdown: false,
+	nextjs: false,
+	node: false,
+	pnpm: false,
+	react: false,
+	regexp: false,
+	solid: false,
+	stylistic: false,
+	svelte: false,
+	test: false,
+	toml: false,
+	typescript: false,
+	e18e: false,
+	unicorn: false,
+	yaml: false,
+};
