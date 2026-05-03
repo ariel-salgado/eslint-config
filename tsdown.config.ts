0d905e8575c688ba9e988a85658d3fc0dc5e3fad
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2026-05-03 00:14:03 -0400

    chore: added publint and arethetypeswrong

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,12 +1,28 @@
+import * as attw from '@arethetypeswrong/core';
+import * as publint from 'publint';
+import * as publint_utils from 'publint/utils';
+
 import { defineConfig } from 'tsdown';
 
 export default defineConfig({
-	entry: ['./src/index.ts'],
+	clean: true,
+	shims: true,
+	exports: true,
+	platform: 'node',
+	entry: [
+		'src/index.ts',
+	],
 	dts: {
 		tsgo: true,
 	},
-	exports: true,
-	clean: true,
-	shims: true,
-	format: ['esm'],
+	publint: {
+		module: [
+			publint,
+			publint_utils,
+		],
+	},
+	attw: {
+		profile: 'esm-only',
+		module: attw,
+	},
 });
